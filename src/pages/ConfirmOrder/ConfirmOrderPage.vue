eac65c8f1732d6fe0051804b19fb9f85485acf98
mangasar7an <mangasaryan1648@gmail.com>
2024-06-18 22:18:38 +0400

    block

--- src/pages/ConfirmOrder/ConfirmOrderPage.vue
+++ src/pages/ConfirmOrder/ConfirmOrderPage.vue
@@ -1,15 +1,242 @@
 <template>
-    <div>
+	<div>
+		<div class="confirm-order container">
+			<h1>Оформление заказа</h1>
+			<div class="confirm-nav">
+                <div class="confirm-nav_item">
+                    <div class="confirm-nav_item-logo_active">
+                        <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.85772 1.74605L7.05743 1.07954C8.26552 0.408381 9.73448 0.408382 10.9426 1.07954L15.9426 3.85732C16.125 3.95868 16.2975 4.0731 16.4591 4.19906L13.0247 6.23112L5.85772 1.74605Z" fill="white"/>
+                        <path d="M4.53606 2.4803L2.05743 3.85732C1.80688 3.99652 1.5751 4.16036 1.36444 4.34485C1.36553 4.34546 1.36662 4.34607 1.3677 4.34668L7.34178 7.70719C8.35948 8.27967 9.60606 8.26219 10.6073 7.66142L10.6113 7.65905L11.5713 7.09107L4.85214 2.88626C4.69605 2.78858 4.5883 2.64288 4.53606 2.4803Z" fill="white"/>
+                        <path d="M0.45005 5.51061C0.159271 6.07051 0 6.70068 0 7.35395V11.6467C0 13.0994 0.787568 14.4379 2.05743 15.1434L7.05743 17.9211C7.437 18.132 7.84233 18.2766 8.25722 18.355C8.25246 18.3208 8.25 18.2859 8.25 18.2505V9.57498C7.6811 9.49213 7.12352 9.30545 6.60637 9.01455L0.632296 5.65403C0.562639 5.61485 0.501655 5.5662 0.45005 5.51061Z" fill="white"/>
+                        <path d="M9.75 9.55424V18.2505C9.75 18.2859 9.74754 18.3208 9.74278 18.355C10.1577 18.2766 10.563 18.132 10.9426 17.9211L15.9426 15.1434C17.2124 14.4379 18 13.0994 18 11.6467V7.35395C18 6.6351 17.8071 5.94422 17.4584 5.34416C17.4343 5.36274 17.4088 5.38007 17.3819 5.39597L11.3791 8.94765L11.377 8.9489C10.8682 9.25381 10.3162 9.45573 9.75 9.55424Z" fill="white"/>
+                        </svg>
+                    </div>
+                    <p class="confirm-nav_item-text_active">Состав заказа</p>
+                </div>
+                <div class="confirm-nav_item confirm-nav_item-border">
+                    <div class="confirm-nav_item-logo">
+                        <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.5 9C11.5 10.3807 10.3807 11.5 9 11.5C7.61929 11.5 6.5 10.3807 6.5 9C6.5 7.61929 7.61929 6.5 9 6.5C10.3807 6.5 11.5 7.61929 11.5 9Z" fill="#EF0000"/>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.4481 19.3019C14.3295 17.1909 17.5544 13.4294 18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0.449096 13.4645 3.34219 17.2505 7.52835 19.3518C8.45077 19.8149 9.54146 19.7951 10.4481 19.3019ZM13 9C13 11.2091 11.2091 13 9 13C6.79086 13 5 11.2091 5 9C5 6.79086 6.79086 5 9 5C11.2091 5 13 6.79086 13 9Z" fill="#EF0000"/>
+                        </svg>
+                    </div>
+                    <p class="confirm-nav_item-text">Состав заказа</p>
+                </div>
+                <div class="confirm-nav_item">
+                    <div class="confirm-nav_item-logo">
+                        <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.9858 3.91224L13.2121 5.85248C12.993 6.40198 13.3977 6.99915 13.9893 6.99915H17.618C18.9407 6.99915 19.8991 8.26016 19.545 9.53458L18.007 15.07C17.526 16.8011 15.9497 17.9992 14.153 17.9992H9.49711C8.76706 17.9992 8.05093 17.7994 7.42633 17.4214L6.17873 16.6665C5.39404 16.1917 4.80938 15.4708 4.49951 14.6363V15.9993C4.49951 16.5516 4.1332 16.9993 3.68133 16.9993H0.817694C0.365824 16.9993 -0.000488281 16.5516 -0.000488281 15.9993V5.99927C-0.000488281 5.44698 0.365824 4.99927 0.817694 4.99927H3.68133C4.1332 4.99927 4.49951 5.44698 4.49951 5.99927V7.56856C4.75622 6.8771 5.20247 6.26107 5.80018 5.79813L6.83177 4.99915L10.621 1.33116C10.8452 1.11413 11.1255 0.963906 11.4304 0.897373C13.1826 0.514973 14.6501 2.24632 13.9858 3.91224Z" fill="#B5B5B5"/>
+                        </svg>
+                    </div>
+                    <p class="confirm-nav_item-text">Состав заказа</p>
+                </div>
+               
+            </div>
+			<div class="confirm-order_content">
+				<div class="confirm-order_grid">
+					<div class="confirm-order_back">
+						<div class="confirm-order_back-up">
+							<h4>Адрес и контакты</h4>
+							<button>Использовать сохраненные данные</button>
+						</div>
+						<form class="confirm-order_back_form">
+							<input
+								type="text"
+								required
+								placeholder="ФИО*"
+								class="confirm-order_back_input-wrong"
+							/>
+							<input
+								type="tel:"
+								required
+								placeholder="Телефон*"
+								class="confirm-order_back_input"
+							/>
+							<input
+								type="text"
+								required
+								placeholder="Страна*"
+								class="confirm-order_back_input"
+							/>
+							<input
+								type="text"
+								required
+								placeholder="Город*"
+								class="confirm-order_back_input"
+							/>
+							<input
+								type="text"
+								required
+								placeholder="Адрес (улица, дом, квартира, индекс)*"
+								class="confirm-order_back_input"
+							/>
+							<div class="">
+								<input
+									type="text"
+									required
+									placeholder="Доп. данные (серия и номер паспорта)*"
+									class="confirm-order_back_input"
+								/>
+								<p class="confirm-order_back_text">
+									Для отправки в транспортных компаниях России
+								</p>
+							</div>
 
-    </div>
+							<input
+								type="text"
+								placeholder="Промокод (при наличии)"
+								class="confirm-order_back_input"
+							/>
+						</form>
+					</div>
+					<div class="confirm-order_row">
+						<div class="confirm-order_back">
+							<h4>Выберите способ доставки</h4>
+							<ul class="confirm-order_list">
+								<li>
+									<input type="radio" name="radio" id="list_radio1" />
+									<label for="list_radio1" class="confirm-order_list-grid">
+										<h4>Авиа</h4>
+										<p>
+											8-15 дней (страховка 1-4%). Одежда сумки 12$, прочее
+											12,5$, обувь 12,5$, майнеры/видео карта 13$, чай/продукты
+											питания 14$
+										</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio"id="list_radio2" />
+									<label for="list_radio2" class="confirm-order_list-grid">
+										<h4>Авто</h4>
+										<p>18-30 дней. От 2.5$ через Казахстан</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio"id="list_radio3" />
+									<label for="list_radio3" class="confirm-order_list-grid">
+										<h4>Авто стандарт</h4>
+										<p>от 35 дней. От 2.1$ за кг</p>
+									</label>
+								</li>
+							</ul>
+						</div>
+						<div class="confirm-order_back">
+							<h4>Страховка груза</h4>
+							<p class="confirm-order_text">
+								Чтобы узнать подробнее про страховку груза и на какие страховые
+								случаи она распространяется, перейдите на
+								<a href="">страницу страхования </a>
+                                <ul class="confirm-order_list">
+								
+								
+								<li>
+									<input type="radio" name="radio1" id="radio1" />
+									<label for="radio1">Нужна страховка</label> 
+								</li>
+                                <li>
+									<input type="radio" name="radio1" id="radio2" />
+									<label for="radio2">Без страховки</label> 
+								</li>
+							</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+                <div class="confirm-order_back">
+					<h4>Выберите способ оплаты заказа</h4>
+					<p class="confirm-order_text">
+						В зависимости от выбранного способа оплаты будет изменена комиссия на выкуп. Мы не берем дополнительную комиссию на оплату. Чтобы узнать подробнее про способы оплаты, перейдите на <a href="">страницу оплаты </a>	
+					</p>
+                    <div class="confirm-order_items">
+                        <ul class="confirm-order_list">
+								<li>
+									<input type="radio" name="radi2" id="list_radio4" />
+									<label for="list_radio4" class="confirm-order_list-grid">
+										<h4>На счет организации (Договор + Торг12)</h4>
+										<p>
+											Комиссия на заказ 3% + 15% налоги (от 200 т.р)
+										</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio2"id="list_radio5" />
+									<label for="list_radio5" class="confirm-order_list-grid">
+										<h4>Наличными в Москве</h4>
+										<p>Комиссия на заказ 3% (от 500 т.р)</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio2"id="list_radio6" />
+									<label for="list_radio6" class="confirm-order_list-grid">
+										<h4>USDT</h4>
+										<p>Через обменники и биржи 3-5%</p>
+									</label>
+								</li>
+                                <li>
+									<input type="radio" name="radio2"id="list_radio7" />
+									<label for="list_radio7" class="confirm-order_list-grid">
+										<h4>Карта Сбербанка 4%/6%</h4>
+										<p>Комиссия на заказ 4% < 10к¥ < 6%. Счёт может быть поделен</p>
+									</label>
+								</li>
+						</ul>
+                        <ul class="confirm-order_list">
+								<li>
+									<input type="radio" name="radio3" id="list_radio8" />
+									<label for="list_radio8" class="confirm-order_list-grid">
+										<h4>Оплата картой-дубликат (от 100 т.р)</h4>
+										<p>
+											Предоставьте дубликат своей карты Сбербанк нашему представителю и при оплате заказа мы снимем необходимую сумму
+										</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio3"id="list_radio9" />
+									<label for="list_radio9" class="confirm-order_list-grid">
+										<h4>Виртуальная карта Мир или QR</h4>
+										<p>от 100 т.р (комиссия заказа от 2.5%)</p>
+									</label>
+								</li>
+								<li>
+									<input type="radio" name="radio3"id="list_radio10" />
+									<label for="list_radio10" class="confirm-order_list-grid">
+										<h4>Наличными в Санкт-Петербурге</h4>
+										<p>От 300 т.р.</p>
+									</label>
+								</li>
+                                
+						</ul>
+                    </div>
+				</div>
+			</div>
+            <div class="confirm-order_block">
+                <div class="confirm-order_block-item">
+                    <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.96335 0.678759L0.76035 9.91189C0.25949 10.8007 0.901725 11.8998 1.92195 11.8998H12.0784C13.0864 11.8998 13.7299 10.8244 13.2533 9.93614L8.29987 0.703001C7.80291 -0.22333 6.47942 -0.237061 5.96335 0.678759ZM7.57529 4.25309C7.5822 3.93127 7.32321 3.66667 7.00132 3.66667C6.67958 3.66667 6.42066 3.93104 6.42735 4.25271L6.50196 7.83574C6.50761 8.10684 6.729 8.32367 7.00016 8.32367C7.2712 8.32367 7.49253 8.10704 7.49835 7.83606L7.57529 4.25309ZM6.52727 10.1272C6.65859 10.2646 6.81616 10.3333 7 10.3333C7.12121 10.3333 7.23131 10.3022 7.3303 10.2399C7.43131 10.1755 7.51212 10.0896 7.57273 9.98229C7.63535 9.87493 7.66667 9.75577 7.66667 9.6248C7.66667 9.43156 7.6 9.26624 7.46667 9.12882C7.33535 8.99141 7.1798 8.9227 7 8.9227C6.81616 8.9227 6.65859 8.99141 6.52727 9.12882C6.39798 9.26624 6.33333 9.43156 6.33333 9.6248C6.33333 9.82233 6.39798 9.9898 6.52727 10.1272Z" fill="#EF0000"/>
+                    </svg>
+                    <p>Внимание: Перед отправкой груза на наш склад уточните у нашего менеджера не является ли товар запрещённым, и сможет ли наша компания отправить его через таможенный пункт (по телефону 8 (800) 707-75-68, или напишите сообщение к этому заказу после добавления)</p>
+                </div>
+                <div class="confirm-order_block-item">
+                    <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.96335 0.678759L0.76035 9.91189C0.25949 10.8007 0.901725 11.8998 1.92195 11.8998H12.0784C13.0864 11.8998 13.7299 10.8244 13.2533 9.93614L8.29987 0.703001C7.80291 -0.22333 6.47942 -0.237061 5.96335 0.678759ZM7.57529 4.25309C7.5822 3.93127 7.32321 3.66667 7.00132 3.66667C6.67958 3.66667 6.42066 3.93104 6.42735 4.25271L6.50196 7.83574C6.50761 8.10684 6.729 8.32367 7.00016 8.32367C7.2712 8.32367 7.49253 8.10704 7.49835 7.83606L7.57529 4.25309ZM6.52727 10.1272C6.65859 10.2646 6.81616 10.3333 7 10.3333C7.12121 10.3333 7.23131 10.3022 7.3303 10.2399C7.43131 10.1755 7.51212 10.0896 7.57273 9.98229C7.63535 9.87493 7.66667 9.75577 7.66667 9.6248C7.66667 9.43156 7.6 9.26624 7.46667 9.12882C7.33535 8.99141 7.1798 8.9227 7 8.9227C6.81616 8.9227 6.65859 8.99141 6.52727 9.12882C6.39798 9.26624 6.33333 9.43156 6.33333 9.6248C6.33333 9.82233 6.39798 9.9898 6.52727 10.1272Z" fill="#EF0000"/>
+                    </svg>
+                    <p>Будьте внимательны. Оплачивайте заказы только по реквизитам, указанным на сайте. Если вам предлагают оплатить счет вне нашего сайта – это мошенники.
+                    </p>
+                </div>
+            </div>
+            <div class="confirm-order_btns">
+                <button class="confirm-order_btn-border">Вернуться назад</button>
+                <button class="confirm-order_btn" disabled>Следующий шаг</button>
+            </div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: 'ConfirmOrderPage'
-    }
+export default {
+	name: 'ConfirmOrderPage',
+}
 </script>
 
-<style src="./confirm-order.css" lang="css" scoped>
-
-</style>
+<style src="./confirm-order.css" lang="css" scoped></style>
